d07dd48f915551d19ad2835f05923591de238311
alexvera1 <sfa.vera@gmail.com>
2022-10-23 21:36:40 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Alexandra Vera on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
